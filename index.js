b21bdd564a692dd10a123a6ac7a786a958eb3305
Tyler Abernathy <54758856+tylerabernathy@users.noreply.github.com>
2024-03-22 18:47:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 function guess() {
     var guess = input.value.toLowerCase();
-    const date = new Date();
-    const players = Object.keys(data)
-    const player = players[date.getDate()]
     input.value = "";
     if (player.toLowerCase() == guess) {
-        document.getElementById("result").innerHTML = "Correct!\nToday's player is " + player + ".";
+        document.getElementById("result").innerHTML = "Correct!\nThe player is " + player + ". Refresh to play again.";
     }
     else {
         document.getElementById("result").innerHTML = "Incorrect. Try again.";
     }
 }
 
+var player = ""
 fetch("./data.json")
     .then(Response => Response.json())
     .then(data => {
-        const date = new Date();
         const players = Object.keys(data)
-        const player = players[date.getDate()]
+        player = players[Math.floor(Math.random() * players.length)]
         document.getElementById("chartDiv").innerHTML = data[player];
     });
 
