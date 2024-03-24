6fc137eba59364359746d0c26bea78b4e07bfe40
Tyler Abernathy <54758856+tylerabernathy@users.noreply.github.com>
2024-03-24 11:21:36 -0500

    Added favicon, play again button, and give up button

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ function guess() {
     var guess = input.value.toLowerCase();
     input.value = "";
     if (player.toLowerCase() == guess) {
-        document.getElementById("result").innerHTML = "Correct!\nThe player is " + player + ". Refresh to play again.";
+        document.getElementById("result").innerHTML = "Correct!\nThe player was " + player + ".";
+        gameOver();
     }
     else {
         document.getElementById("result").innerHTML = "Incorrect. Try again.";
     }
+}
+
+function giveUp() {
+    document.getElementById("result").innerHTML = "The player was " + player + ".";
+    gameOver();
+}
+
+function gameOver() {
+    document.getElementById("guessBox").style.visibility = "hidden";
+    document.getElementById("guessButton").style.visibility = "hidden";
+    document.getElementById("giveUpButton").style.visibility = "hidden";
+    document.getElementById("retryButton").style.visibility = "visible";
 }
 
 var player = ""
